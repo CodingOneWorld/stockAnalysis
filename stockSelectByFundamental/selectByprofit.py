9687280caa157a199f54939247a7aaec4c1bdf30
zq <601041109@qq.com>
2020-07-23 19:57:02 +0800

    增加筛选收入、利润持续增长的股票

--- stockSelectByFundamental/selectByprofit.py
+++ stockSelectByFundamental/selectByprofit.py
@@ -17,25 +17,25 @@ filepath = 'E:/Money/stocks.db'
 
 # pandas连接数据库
 conn = sqlite3.connect(filepath)
-incomeData = pd.read_sql('select * from incomeIn5years', conn)
-print(incomeData.head())
+profitData = pd.read_sql('select * from profitIn5years', conn)
+print(profitData.head())
 
 # 计算每年的收入增长幅度
 # 计算每年的收入增长率
 for i in range(4):
-    # 增长幅度
-    incomeData['change' + str(i + 1)] = incomeData['business_income' + str(2015 + i + 1)] - incomeData[
-        'business_income' + str(2015 + i)]
-    # 增长率
-    incomeData['pct_change' + str(i + 1)] = (incomeData['business_income' + str(2015 + i + 1)] - incomeData[
-        'business_income' + str(2015 + i)]) / incomeData['business_income' + str(2015 + i + 1)]
-print(incomeData.head())
+    # 收入增长幅度
+    profitData['change' + str(i + 1)] = profitData['net_profits' + str(2015 + i + 1)] - profitData[
+        'net_profits' + str(2015 + i)]
+    # 收入增长率
+    profitData['pct_change' + str(i + 1)] = (profitData['net_profits' + str(2015 + i + 1)] - profitData[
+        'net_profits' + str(2015 + i)]) / profitData['net_profits' + str(2015 + i)]
+print(profitData.head())
 
 # 筛选近五年收入持续增长的股票
 data = \
-    incomeData.loc[incomeData['change1'] > 0].loc[incomeData['change2'] > 0].loc[
-        incomeData['change3'] > 0].loc[
-        incomeData['change4'] > 0]
+    profitData.loc[profitData['change1'] > 0].loc[profitData['change2'] > 0].loc[
+        profitData['change3'] > 0].loc[
+        profitData['change4'] > 0]
 print(data.count())
 
 data2 = \
