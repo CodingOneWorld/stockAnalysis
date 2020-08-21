6421b8a9ad44767a0f4e4ce73ca8ef92ea44616c
zq <601041109@qq.com>
2020-08-21 17:58:33 +0800

    基本面选股

--- tradeData/main.py
+++ tradeData/main.py
@@ -4,7 +4,7 @@ from tradeData.updateDailyData_tspro import updateDailyData_tspro
 from tradeData.getStockBasicList import getStockBasicList
 
 # 参数
-update_date = "20200728"
+update_date = "20200822"
 
 # db path
 filepath = 'E:/Money/stocks.db'
