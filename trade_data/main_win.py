9663b68fc67ab678dc55602417e2e44941c2b1a8
zq <601041109@qq.com>
2021-04-26 11:47:45 +0800

    定时任务,区分mac与 win

---
+++ trade_data/main_win.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import sys
+
+# 参数
+sys.path.append('E:\\coding\\pycharmProjects\\stockAnalysis\\trade_data')
+sys.path.append('E:\\coding\\pycharmProjects\\stockAnalysis\\contants')
+sys.path.append('E:\\coding\\pycharmProjects\\stockAnalysis')
+
+
+import sqlite3
+from contants.commonContants import DB_PATH
+from trade_data.updateDailyTradeData import updateDailyData_tspro
+from trade_data.getStockBasicList import getStockBasicList_tspro
+import pandas as pd
+import datetime
+
+
+# 从数据库中直接推算日期参数
+# update_date = "20201118"
+# pandas连接数据库
+conn = sqlite3.connect(DB_PATH)
+# 读取相应的交易数据表
+table_name = 'S000001_daily'
+stock_trade_data = pd.read_sql('select * from ' + table_name, conn)
+trade_date_arr = sorted(stock_trade_data['trade_date'].values, reverse=True)
+day0 = datetime.datetime.strptime(str(trade_date_arr[0]), '%Y%m%d')
+delta_1d = datetime.timedelta(days=1)
+update_date = (day0 + delta_1d).strftime('%Y%m%d')
+print(update_date)
+
+updateDailyData_tspro(update_date, DB_PATH, 0, 0, 0)
+getStockBasicList_tspro(DB_PATH)
