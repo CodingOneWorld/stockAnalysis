3fb1b403451352f104b10305c541a1759f7ebd88
“zhangqi105git config --global user.name “zhangqi105 <zhangqi105@maoyan.com>
2021-07-12 19:38:39 +0800

    均线比较

--- analysis_util/cal_mean_line.py
+++ analysis_util/cal_mean_line.py
@@ -16,7 +16,7 @@ pd.set_option('max_colwidth', 200)
 pd.set_option('expand_frame_repr', False)
 
 
-def cal_mean_line(code,latest_days):
+def cal_mean_line(code, latest_days):
     # 连接sqlite数据库
     conn = sqlite3.connect(DB_PATH)
     # 读取相应的交易数据表
@@ -26,8 +26,65 @@ def cal_mean_line(code,latest_days):
     stock_trade_data.set_index("trade_date", inplace=True)
 
     latest_days = latest_days if len(stock_trade_data.close) > latest_days else len(stock_trade_data.close)
-    start_days=len(stock_trade_data.close)-latest_days+1
-    stock_trade_data=stock_trade_data.iloc[start_days:len(stock_trade_data.close), :]
+    start_days = len(stock_trade_data.close) - latest_days + 1
+    stock_trade_data = stock_trade_data.iloc[start_days:len(stock_trade_data.close), :]
+
+    # 绘制交易金额曲线
+    stock_trade_data.loc[:, 'low'].plot.line()
+
+    # 计算移动均线，根据收盘价
+    df_mean = stock_trade_data.close.rolling(window=10).mean().fillna(0)
+    # print(df_mean)
+    mean_10_latest = df_mean.values[-1]
+    # print(mean_10_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=20).mean().fillna(0)
+    # print(df_mean)
+    mean_20_latest = df_mean.values[-1]
+    # print(mean_20_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=30).mean().fillna(0)
+    # print(df_mean)
+    mean_30_latest = df_mean.values[-1]
+    # print(mean_30_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=60).mean().fillna(0)
+    # print(df_mean)
+    mean_60_latest = df_mean.values[-1]
+    # print(mean_60_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=88).mean().fillna(0)
+    mean_88_latest = df_mean.values[-1]
+    # print(mean_88_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=120).mean().fillna(0)
+    mean_120_latest = df_mean.values[-1]
+    # print(mean_120_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=140).mean().fillna(0)
+    mean_140_latest = df_mean.values[-1]
+    # print(mean_140_latest)
+
+    df_mean = stock_trade_data.close.rolling(window=180).mean().fillna(0)
+    mean_180_latest = df_mean.values[-1]
+    # print(mean_180_latest)
+    return (
+    mean_10_latest, mean_20_latest, mean_30_latest, mean_60_latest, mean_88_latest, mean_120_latest, mean_140_latest,
+    mean_180_latest)
+
+
+def plot_mean_line(code, latest_days):
+    # 连接sqlite数据库
+    conn = sqlite3.connect(DB_PATH)
+    # 读取相应的交易数据表
+    table_name = 'S' + code + '_daily'
+    stock_trade_data = pd.read_sql('select * from ' + table_name, conn)
+    stock_trade_data['trade_date'] = stock_trade_data['trade_date'].apply(lambda x: str(x))
+    stock_trade_data.set_index("trade_date", inplace=True)
+
+    latest_days = latest_days if len(stock_trade_data.close) > latest_days else len(stock_trade_data.close)
+    start_days = len(stock_trade_data.close) - latest_days + 1
+    stock_trade_data = stock_trade_data.iloc[start_days:len(stock_trade_data.close), :]
 
     # 绘制交易金额曲线
     stock_trade_data.loc[:, 'low'].plot.line()
@@ -60,49 +117,7 @@ def cal_mean_line(code,latest_days):
     plt.show()
 
 
-def plot_mean_line(code,latest_days):
-    # 连接sqlite数据库
-    conn = sqlite3.connect(DB_PATH)
-    # 读取相应的交易数据表
-    table_name = 'S' + code + '_daily'
-    stock_trade_data = pd.read_sql('select * from ' + table_name, conn)
-    stock_trade_data['trade_date'] = stock_trade_data['trade_date'].apply(lambda x: str(x))
-    stock_trade_data.set_index("trade_date", inplace=True)
+if __name__ == '__main__':
+    mean_list = cal_mean_line('000001', 300)
+    print(mean_list)
 
-    latest_days = latest_days if len(stock_trade_data.close) > latest_days else len(stock_trade_data.close)
-    start_days=len(stock_trade_data.close)-latest_days+1
-    stock_trade_data=stock_trade_data.iloc[start_days:len(stock_trade_data.close), :]
-
-    # 绘制交易金额曲线
-    stock_trade_data.loc[:, 'low'].plot.line()
-
-    # 计算移动均线，根据收盘价
-    df_mean = stock_trade_data.close.rolling(window=60).mean().fillna(0)
-    # print(df_mean)
-    df_mean.plot.line()
-
-    df_mean = stock_trade_data.close.rolling(window=140).mean().fillna(0)
-    # print(df_mean)
-    df_mean.plot.line()
-    #
-    # df_mean = stock_trade_data.close.rolling(window=88).mean().fillna(0)
-    # # print(df_mean)
-    # df_mean.plot.line()
-    #
-    # df_mean = stock_trade_data.close.rolling(window=120).mean().fillna(0)
-    # # print(df_mean)
-    # df_mean.plot.line()
-    #
-    # df_mean = stock_trade_data.close.rolling(window=140).mean().fillna(0)
-    # # print(df_mean)
-    # df_mean.plot.line()
-
-    # df_mean = stock_trade_data.close.rolling(window=180).mean().fillna(0)
-    # # print(df_mean)
-    # df_mean.plot.line()
-
-    plt.show()
-
-
-
-cal_mean_line('000001',300)
--- stock_select/key_price_compare.py
+++ stock_select/key_price_compare.py
@@ -5,5 +5,12 @@
 # 计算当前价格最高值，最低值，以及均线数据，进行比较
 from analysis_util.cal_mean_line import cal_mean_line
 
-cal_mean_line('000001',3000)
+if __name__ == '__main__':
+    mean_list = cal_mean_line('000001', 3000)
+    print(mean_list)
+
+    # 获取股票最近的价格
+
+
+
 
