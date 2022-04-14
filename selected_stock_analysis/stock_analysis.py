e49b272444e1a76ee2787dfdeab0ff11f825318b
“zhangqi105git config --global user.name “zhangqi105 <zhangqi105@maoyan.com>
2022-04-14 22:03:10 +0800

    自选股筛选分类

--- selected_stock_analysis/stock_analysis.py
+++ selected_stock_analysis/stock_analysis.py
@@ -5,72 +5,16 @@ import sqlite3
 import pandas as pd
 import numpy as np
 import tushare as ts
+
+from analysis_util.plot_k_line import plot_k_line
 from contants.common_contants import DB_PATH
 from analysis_util.cal_stock_trend import cal_stock_price_trend,cal_trend_common
 from stock_select.get_low_price_stock import cal_low_price_stock
 
 if __name__ == '__main__':
-    # 获取自选股列表
-    fr = open('自选股.txt', 'r',encoding='utf-8')
-    stock_list = []
-    for line in fr:
-        line = line.strip()
-        line = line.split(',')
-        stock_list.append(line)
-    # stock_list = np.array(stock_list)
-    print(stock_list[0])
-
-    # 趋势筛选，处于上升趋势
-    # # 超短线，最近3、5日处于上升趋势
-    # super_short_stocks=[]
-    # # 短线，最近5、10日处于上升趋势
-    short_stocks=[]
-    # # 中线，最近10，30日处于上升趋势
-    # medium_stocks=[]
-    # # 中长线，最近3个月，6个月处于上升通道
-    # medium_long_stocks=[]
-    # 长线
-    print("长线")
-    long_stock=[]
-    for line in stock_list:
-        s=line[0]
-    #     k3 = cal_stock_price_trend(s, 3)
-        k5 = cal_stock_price_trend(s, 5)
-        k10 = cal_stock_price_trend(s, 10)
-    #     k30 = cal_stock_price_trend(s, 30)
-        # k60 = cal_stock_price_trend(s, 60)
-    #     k120 = cal_stock_price_trend(s, 120)
-    #     k300 = cal_stock_price_trend(s, 300)
-    #     k3000 = cal_stock_price_trend(s, 4000)
-    #     if k3 > 0.3 and k5>0.3:
-    #         # print(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #         super_short_stocks.append(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-        if k5 > 0.3 and k10>0.3:
-            # print(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-            # short_stocks.append(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-            short_stocks.append(line[1])
-            print(line)
-    #     if k30 > 0.3 and k10>0.3:
-    #         # print(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #         medium_stocks.append(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #     if k60 > 0.3 and k120>0.3:
-    #         # print(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #         medium_long_stocks.append(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #     if k3000>0.1:
-    #         # print(stock_list_data[stock_list_data["symbol"] == s]['name'].values[0])
-    #         long_stock.append(line[1])
-    #         print(line)
-    # print("趋势股：")
-    # print("超短线：")
-    # print(super_short_stocks)
-    print("短线：")
-    print(short_stocks)
-    # print("中线：")
-    # print(medium_stocks)
-    # print("中长线：")
-    # print(medium_long_stocks)
-    # print("长线数：")
-    # print(len(long_stock))
+    plot_k_line('000895')
+    k=cal_stock_price_trend('000895', 3)
+    print(k)
 
 
 
---
+++ selected_stock_analysis/stock_classification.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# 将股票进行分类，即自选股分板块，如上升通道，低价股等等
+
+import pandas as pd
+import numpy as np
+
+# 获取短期上升通道股票，5
+from analysis_util.cal_stock_trend import cal_stock_price_trend
+
+
+def get_latest_rising_stocks():
+    grad1 = 0.1
+    grad2 = 0.2
+    grad3 = 0.3
+    # 获取自选股票池
+    df = pd.read_csv('自选股.csv', dtype={'symbol': np.str})
+    # df['symbol']=df['symbol'].astype('string')
+    stock_list = df.values
+    print(stock_list)
+    # 趋势筛选，处于上升趋势
+    # 超短线，最近3、5日处于上升趋势
+    super_short_stocks = []
+    # 短线，最近5、10日处于上升趋势
+    short_stocks = []
+    # 中线，最近10，30日处于上升趋势
+    medium_stocks = []
+    # 中长线，最近3个月，6个月处于上升通道
+    medium_long_stocks = []
+    # 长线
+    long_stock = []
+    for line in stock_list:
+        print(line)
+        s = line[0]
+        k3 = cal_stock_price_trend(s, 3)
+        k5 = cal_stock_price_trend(s, 5)
+        k10 = cal_stock_price_trend(s, 10)
+        k30 = cal_stock_price_trend(s, 30)
+        k60 = cal_stock_price_trend(s, 60)
+        # k90 = cal_stock_price_trend(s, 90)
+        k120 = cal_stock_price_trend(s, 120)
+        # k180 = cal_stock_price_trend(s, 180)
+        # k300 = cal_stock_price_trend(s, 300)
+        k3000 = cal_stock_price_trend(s, 4000)
+        if k3 > grad2 and k5 > grad2:
+            super_short_stocks.append(line)
+        if k5 > grad2 and k10 > grad2:
+            short_stocks.append(line)
+        if k30 > grad2 and k10 > grad2:
+            medium_stocks.append(line)
+        if k60 > grad2 and k120 > grad2:
+            medium_long_stocks.append(line)
+        if k3000 > 0.1:
+            long_stock.append(line)
+    print("趋势股：")
+    print("超短线：")
+    print(super_short_stocks)
+    print("短线：")
+    print(short_stocks)
+    print("中线：")
+    print(medium_stocks)
+    print("中长线：")
+    print(medium_long_stocks)
+    print("长线数：")
+    print(long_stock)
+
+
+get_latest_rising_stocks()
