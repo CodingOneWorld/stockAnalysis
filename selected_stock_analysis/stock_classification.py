6efe7a23f029346a12f0304c7784411b773f29fb
“zhangqi105git config --global user.name “zhangqi105 <zhangqi105@maoyan.com>
2022-06-07 19:24:43 +0800

    股票池筛选

--- selected_stock_analysis/stock_classification.py
+++ selected_stock_analysis/stock_classification.py
@@ -8,16 +8,16 @@ import numpy as np
 # 获取短期上升通道股票，5
 from analysis_util.cal_stock_trend import cal_stock_price_trend
 
-
-def get_latest_rising_stocks():
-    grad1 = 0.1
-    grad2 = 0.2
-    grad3 = 0.3
+def classify_stocks():
     # 获取自选股票池
     df = pd.read_csv('自选股.csv', dtype={'symbol': np.str})
     # df['symbol']=df['symbol'].astype('string')
     stock_list = df.values
     print(stock_list)
+    grad1 = 0.1
+    grad2 = 0.2
+    grad3 = 0.3
+
     # 趋势筛选，处于上升趋势
     # 超短线，最近3、5日处于上升趋势
     super_short_stocks = []
@@ -29,6 +29,16 @@ def get_latest_rising_stocks():
     medium_long_stocks = []
     # 长线
     long_stock = []
+
+    # 反弹股
+    # 刚开始反弹
+    rebound_stocks_l=[]
+    # 中期反弹
+    rebound_stocks_m=[]
+    # 晚期反弹
+    rebound_stocks_h=[]
+
+    # 反弹股
     for line in stock_list:
         print(line)
         s = line[0]
@@ -37,11 +47,13 @@ def get_latest_rising_stocks():
         k10 = cal_stock_price_trend(s, 10)
         k30 = cal_stock_price_trend(s, 30)
         k60 = cal_stock_price_trend(s, 60)
-        # k90 = cal_stock_price_trend(s, 90)
+        k90 = cal_stock_price_trend(s, 90)
         k120 = cal_stock_price_trend(s, 120)
         # k180 = cal_stock_price_trend(s, 180)
         # k300 = cal_stock_price_trend(s, 300)
         k3000 = cal_stock_price_trend(s, 4000)
+
+        # 上升趋势
         if k3 > grad2 and k5 > grad2:
             super_short_stocks.append(line)
         if k5 > grad2 and k10 > grad2:
@@ -52,27 +64,48 @@ def get_latest_rising_stocks():
             medium_long_stocks.append(line)
         if k3000 > grad1:
             long_stock.append(line)
+
+        # 反弹
+        if k30 <0 and k5 > grad1:
+            rebound_stocks_l.append(line)
+        if k60 <0 and k10 > grad1:
+            rebound_stocks_m.append(line)
+        if k90 <0 and k30 > grad1:
+            rebound_stocks_h.append(line)
     print("趋势股：")
     print("超短线上升通道：")
     super_short_stocks = pd.DataFrame(super_short_stocks, columns=['symbol', 'stock_name'])
-    super_short_stocks.to_csv('超短线上升通道.csv',index=0)
+    super_short_stocks.to_csv('超短线上升通道.csv', index=0)
     print(super_short_stocks)
     print("短线上升通道：")
     short_stocks = pd.DataFrame(short_stocks, columns=['symbol', 'stock_name'])
-    super_short_stocks.to_csv('短线上升通道.csv',index=0)
+    short_stocks.to_csv('短线上升通道.csv', index=0)
     print(short_stocks)
     print("中线上升通道：")
     medium_stocks = pd.DataFrame(medium_stocks, columns=['symbol', 'stock_name'])
-    super_short_stocks.to_csv('中线上升通道.csv',index=0)
+    medium_stocks.to_csv('中线上升通道.csv', index=0)
     print(medium_stocks)
     print("中长线上升通道：")
     medium_long_stocks = pd.DataFrame(medium_long_stocks, columns=['symbol', 'stock_name'])
-    super_short_stocks.to_csv('中长线上升通道.csv',index=0)
+    medium_long_stocks.to_csv('中长线上升通道.csv', index=0)
     print(medium_long_stocks)
     print("长线上升通道：")
     long_stock = pd.DataFrame(long_stock, columns=['symbol', 'stock_name'])
-    super_short_stocks.to_csv('长线上升通道.csv',index=0)
+    long_stock.to_csv('长线上升通道.csv', index=0)
     print(long_stock)
 
+    print("反弹股：")
+    print("开始反弹：")
+    rebound_stocks_l = pd.DataFrame(rebound_stocks_l, columns=['symbol', 'stock_name'])
+    rebound_stocks_l.to_csv('开始反弹.csv', index=0)
+    print(rebound_stocks_l)
+    print("中期反弹：")
+    rebound_stocks_m = pd.DataFrame(rebound_stocks_m, columns=['symbol', 'stock_name'])
+    rebound_stocks_m.to_csv('中期反弹.csv', index=0)
+    print(rebound_stocks_m)
+    print("晚期反弹：")
+    rebound_stocks_h = pd.DataFrame(rebound_stocks_h, columns=['symbol', 'stock_name'])
+    rebound_stocks_h.to_csv('晚期反弹.csv', index=0)
+    print(rebound_stocks_h)
 
-get_latest_rising_stocks()
+classify_stocks()
