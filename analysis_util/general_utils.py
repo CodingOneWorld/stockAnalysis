8abb5c718424ac00d9eb89beb162f944971ce6b0
“zhangqi105git config --global user.name “zhangqi105 <zhangqi105@maoyan.com>
2023-05-31 18:06:56 +0800

    股票池筛选，自选股监控 分析结果输出到word

--- analysis_util/general_utils.py
+++ analysis_util/general_utils.py
@@ -6,27 +6,21 @@ from constants.common_constants import DB_PATH
 from trade_data.get_stock_basic_list import get_stock_basic_list
 
 
-def get_stock_name(stock_code,mode='online'):
-    if mode == 'online':
-        stock_list_data=get_stock_basic_list()
-    else:
-        # 读取全部股票数据库
-        conn = sqlite3.connect(DB_PATH)
-        # 读取股票基本信息表
-        stock_list_data = pd.read_sql('select * from stock_list', conn)
+def get_stock_name(stock_code,mode='file'):
+    if mode == 'file':
+        stock_list_data=get_stock_basic_list('file')
+    elif mode=='DB':
+        stock_list_data=get_stock_basic_list('DB')
     return stock_list_data[stock_list_data['symbol']==stock_code]['name'].values[0]
 
 
-def get_stock_code(stock_name,mode='online'):
-    if mode == 'online':
-        stock_list_data=get_stock_basic_list()
-    else:
-        # 读取全部股票数据库
-        conn = sqlite3.connect(DB_PATH)
-        # 读取股票基本信息表
-        stock_list_data = pd.read_sql('select * from stock_list', conn)
+def get_stock_code(stock_name,mode='file'):
+    if mode == 'file':
+        stock_list_data=get_stock_basic_list('file')
+    elif mode == 'DB':
+        stock_list_data = get_stock_basic_list('DB')
     return stock_list_data[stock_list_data['name']==stock_name]['symbol'].values[0]
 
 
 if __name__ == '__main__':
-    print(get_stock_code('超声电子'))
+    print(get_stock_code('南方传媒'))
--- analysis_util/plot_k_line.py
+++ analysis_util/plot_k_line.py
@@ -97,7 +97,7 @@ def save_k_line(symbol,latest_days,save_path):
 
 
 if __name__ == '__main__':
-    s=get_stock_code('成飞集成')
-    # plot_k_line_latestdays(s,100)
-    plot_k_line(s,'20190301','20201231')
+    s=get_stock_code('三一重工')
+    plot_k_line_latestdays(s,300)
+    # plot_k_line(s,'20190301','20201231')
 
